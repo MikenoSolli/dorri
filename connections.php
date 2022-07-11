42decab376229aa57cdf8b9df1f7e335911dff85
MikenoSolli <97721861+MikenoSolli@users.noreply.github.com>
2022-07-11 17:44:41 +0300

    Update connections.php

--- connections.php
+++ connections.php
@@ -1,10 +1,14 @@
 <?php
+$dsn = "pgsql:"
+    . "host=ec2-54-159-22-90.compute-1.amazonaws.com;"
+    . "dbname=d3lu7kruu6l1ni;"
+    . "user=gqcadqoagowdjf;"
+    . "port=5432;"
+    . "sslmode=require;"
+    . "password=f1f30ab292770253e7f0ce37aa2720a605a5660569134ed03287002bbf01d59f";
 
-$host = "ec2-54-159-22-90.compute-1.amazonaws.com"; //IP of your database
-$user = "gqcadqoagowdjf"; //Username for database login
-$pass = "f1f30ab292770253e7f0ce37aa2720a605a5660569134ed03287002bbf01d59f"; //Password associated with the username
-$database = "d3lu7kruu6l1ni"; //Your database name
-
-$connectQuery = new PDO("pgsql:host=$host;dbname=$database", $user, $pass);
+$connectQuery = new PDO($dsn);
+//$connectQuery = new PDO("pgsql:host=$host;dbname=$database", $user, $pass);
 
 ?>
+
