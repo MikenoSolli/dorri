1a550042ef70eb1037c628bd1f97a6e8c7ed35e5
MikenoSolli <97721861+MikenoSolli@users.noreply.github.com>
2022-07-11 17:00:29 +0300

    Update connections.php

--- connections.php
+++ connections.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost"; //IP of your database
-$user = "postgres"; //Username for database login
-$pass = "Yesupekee"; //Password associated with the username
-$database = "postgres"; //Your database name
+$host = "ec2-54-159-22-90.compute-1.amazonaws.com"; //IP of your database
+$user = "gqcadqoagowdjf"; //Username for database login
+$pass = "f1f30ab292770253e7f0ce37aa2720a605a5660569134ed03287002bbf01d59f"; //Password associated with the username
+$database = "d3lu7kruu6l1ni"; //Your database name
 
 $connectQuery = new PDO("pgsql:host=$host;dbname=$database", $user, $pass);
 
